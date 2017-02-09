ac9229338c18d8c10a32af596c7cfa81a1afff1d
Alajuwon Thomas <spooda7@yahoo.com>
2017-02-09 12:05:31 -0500

    added a comments controller with views and feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do 
 
+ 	
+  get 'comments' => 'comment#feed'
 
-  devise_for :users
+  get 'views' => 'comment#view'
+
+
   root 'welcome#index'
 
  
-
+devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
