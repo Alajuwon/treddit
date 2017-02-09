8661d8dadba658c2f00eaa41bc77991175b2e0c4
bclayday <taylorhvacatl@gmail.com>
2017-02-09 12:18:28 -0500

    bruce

--- Gemfile
+++ Gemfile
@@ -20,7 +20,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'bootstrap-sass'
 gem 'timeago-rails'
-
 gem 'font-awesome-rails'
 gem 'devise'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery  
 //= require jquery_ujs
 //= require turbolinks
 //= require boottsrap-sprockets
-//=require timeago
+//= require timeago
 //= require_tree .
